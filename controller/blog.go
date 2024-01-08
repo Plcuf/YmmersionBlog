806bb7d6bec32565503e8ffd4b80c309b9e87be3
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-01-08 21:26:40 +0100

    update

--- controller/blog.go
+++ controller/blog.go
@@ -4,16 +4,25 @@ import (
 	InitStruct "Ymmersion2/backend"
 	InitTemp "Ymmersion2/temps"
 	"net/http"
-) 
+	"fmt"
+)
 
 func Accueil(w http.ResponseWriter, r *http.Request) {	
-	
+	var err error
+	InitStruct.Articles, err = InitStruct.ReadJSON()
+	if err != nil {
+		fmt.Println("Error encodage ", err.Error())
+		return
+	}
+	InitStruct.Articles.Jeux[0].Name = "caca"
+	InitStruct.EditJSON(InitStruct.Articles)
 	InitTemp.Temp.ExecuteTemplate(w, "Accueil", nil)
 }
 
 
 
 func Detail(w http.ResponseWriter, r *http.Request) {
+
 	InitTemp.Temp.ExecuteTemplate(w, "Detail", nil)
 }
 
