af2549c12a123d17e51370fd3e4d8c7112460c80
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-01-11 19:36:15 +0100

    clear print / template regler

--- controller/blog.go
+++ controller/blog.go
@@ -52,7 +52,6 @@ func Detail(w http.ResponseWriter, r *http.Request) {
 	}
 	InitStruct.UserData.Url = r.URL.String()
 	InitStruct.Back.Articles = []InitStruct.Article{InitStruct.Section}
-	fmt.Println(InitStruct.Back.Articles[0].Category)
 	//execution du template Detail.html
 	InitTemp.Temp.ExecuteTemplate(w, "Detail", InitStruct.Back)
 }
